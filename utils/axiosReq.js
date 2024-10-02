990f04720265a93eaa056f0b36dfb001602b6694
hamim <hamim2114@gmail.com>
2024-10-02 17:49:15 +0600

    updated

--- utils/axiosReq.js
+++ utils/axiosReq.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost'
 ? 'http://localhost:5000/api'
-: 'https://api.poshcoder.com/api';
+: 'https://edu-quest-server.vercel.app/api';
 
 export const axiosReq = axios.create({
   baseURL: baseURL,
-  // withCredentials: true
+  withCredentials: true
 });
